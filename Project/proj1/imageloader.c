fe9fb70c86ea6639dc7805cd2ab60fe56f09dd58
Hassan Mahmoud <officialhassansmail@gmail.com>
2025-10-01 17:43:09 +0300

    - progress in project 1

--- Project/proj1/imageloader.c
+++ Project/proj1/imageloader.c
@@ -25,7 +25,37 @@
 //Make sure that you close the file with fclose before returning.
 Image *readData(char *filename) 
 {
-	//YOUR CODE HERE
+
+	FILE* fp = fopen(filename, "r");
+
+	if (fp == NULL) {
+		printf("Failed to open %s file", filename);
+		exit(EXIT_FAILURE);
+	}
+
+	// discard the P3 header
+	fscanf(fp, "%*s");
+
+	// read the width and height
+	int width, height;
+	fscanf(fp, "%d %d", &width, &height);
+
+	// initialize the image
+	Image* img = malloc(sizeof (Image));
+	img->rows = width;
+	img->cols = height;
+
+	img->image = malloc(width * sizeof(Color*));
+	for (int i = 0; i < height; i++) {
+		img->image[i] = malloc(height * sizeof(Color));
+	}
+
+
+
+
+	// fclose(fp);
+	// return ... 
+
 }
 
 //Given an image, prints to stdout (e.g. with printf) a .ppm P3 file with the image's data.
