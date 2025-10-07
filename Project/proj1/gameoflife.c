b429dd3e65f76d711ba38ac0c1f34eb65241ff34
Hassan Mahmoud <officialhassansmail@gmail.com>
2025-10-07 14:16:32 +0300

    - solve colors problem, project 0 is DONE.

--- Project/proj1/gameoflife.c
+++ Project/proj1/gameoflife.c
@@ -5,23 +5,24 @@
 ** DESCRIPTION: CS61C Fall 2020 Project 1
 **
 ** AUTHOR:      Justin Yokota - Starter Code
-**				YOUR NAME HERE
+**				Hassan Siddig Mahmoud
 **
 **
 ** DATE:        2020-08-23
 **
 **************************************************************************/
 
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <inttypes.h>
 #include "imageloader.h"
 
 // decode the rule into an 18 elements array (1 lives, 0 dies)
-int* get_rules(int rule) {
+uint32_t* get_rules(int rule) {
 
 	int num_of_rules = 18;
-	int *result = malloc(num_of_rules * sizeof(int));
+	uint32_t *result = malloc(num_of_rules * sizeof(uint32_t));
 
 	if (result == NULL) {
 		printf("Memory allocation failed\n");
@@ -46,50 +47,68 @@ int floor_mod(int a, int b) {
 //Determines what color the cell at the given row/col should be. This function allocates space for a new Color.
 //Note that you will need to read the eight neighbors of the cell in question. The grid "wraps", so we treat the top row as adjacent to the bottom row
 //and the left column as adjacent to the right column.
-Color *evaluateOneCell(Image *image, int row, int col, uint32_t rule)
+Color *evaluateOneCell(Image *image, int row, int col, uint32_t *rule_arr)
 {
 	// get the rules in an array for fast lookup
 	// in a cleaner math:
 	// int index = (state * 9) + neighbors; // state = 0 || 1
 	// int next = (rule >> index) & 1;
-	int *rule_arr = get_rules(rule);
 
 	int rows = image->rows;
 	int cols = image->cols;
 
-	// evaluate the neighbours
-	int alive = 0;
-	for (int i = -1; i <= 1; i++) {
-		for (int j = -1; j <= 1; j++) {
+	Color* new_color = malloc(sizeof(Color));
+	new_color->R = 0;
+	new_color->G = 0;
+	new_color->B = 0;
 
-			// skip the cell itself
-			if (i == 0 && j == 0) {
-				continue;
+	// evaluate each channel bits independently
+	for (int channel = 0; channel < 3; channel++) {
+		for (int bit = 0; bit < 8; bit++) {
+
+			int alive = 0;
+
+			int mask = 1 << bit;
+
+			for (int r = -1; r <= 1; r++) {
+				for (int c = -1; c <= 1; c++) {
+
+					if (r == 0 && c == 0) continue;
+
+					int new_row = floor_mod(row + r, rows);
+					int new_col = floor_mod(col + c, cols);
+					Color neigh = image->image[new_row][new_col];
+
+					// pick the channel that we are working on
+					uint8_t neigh_channel = (channel == 0) ? neigh.R :
+															(channel == 1) ? neigh.G : neigh.B;
+					
+					// check only this bit
+					if (neigh_channel & mask) {
+						alive++;
+					}
+					
+				}
 			}
 
-			int new_row = floor_mod(row + i, rows);
-			int new_col = floor_mod(col + j, cols);
-			Color neighbour = image->image[new_row][new_col];
-			alive += neighbour.R || neighbour.G || neighbour.B;
+			// determine the state of that bit
+			Color cur = image->image[row][col];
+			uint8_t cur_channel = channel == 0 ? cur.R : channel == 1 ? cur.G : cur.B;
+			int curr_state = (cur_channel & mask) ? 1 : 0;
+			int new_state = rule_arr[(curr_state ? 9 : 0) + alive];
+
+			// set this bit in the new color
+			if (new_state) {
+				if (channel == 0) {
+					new_color->R |= mask;
+				} else if (channel == 1) {
+					new_color->G |= mask;
+				} else {
+					new_color->B |= mask;
+				}
+			}
+
 		}
-	}
-
-	// the state of the cell itself
-	Color color = image->image[row][col];
-	int is_alive = color.R || color.G || color.B;
-
-	// check the rule
-	int new_state = rule_arr[(is_alive ? 9 : 0) + alive];
-
-	// the new state
-	int black = 0;
-	int white = 255;
-	Color* new_color = malloc(sizeof(Color));
-
-	if (new_state) {
-		new_color->R = new_color->G = new_color->B = white;
-	} else {
-		new_color->R = new_color->G = new_color->B = black;
 	}
 
 	free(rule_arr);
@@ -125,13 +144,17 @@ Image *life(Image *image, uint32_t rule)
 		new_image->image[i] = malloc(cols * sizeof(Color));
 	}
 
+	uint32_t *rule_arr = get_rules(rule);
+
 	for (int i = 0; i < rows; i++) {
 		for (int j = 0; j < cols; j++) {
-			Color* color_ptr = evaluateOneCell(image, i, j, rule);
+			Color* color_ptr = evaluateOneCell(image, i, j, rule_arr);
 			new_image->image[i][j] = *color_ptr;
 			free(color_ptr);
 		}
 	}
+
+	free(rule_arr);
 
 	return new_image;
 }
