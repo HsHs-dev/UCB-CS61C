92498439639d9e21f2cec398bd1a0c084bd118c8
Hassan Mahmoud <officialhassansmail@gmail.com>
2025-10-14 21:39:41 +0300

    - solve ex3 lab3: factorial

--- Lab/lab03/factorial.s
+++ Lab/lab03/factorial.s
@@ -23,18 +23,17 @@ main:
 factorial:
     addi t0, x0, 2
     blt a0, t0, exit # if a0 is less than 2, jump to exit
-    addi s0, x0, a0
-    addi sp, sp, -8
+    add s0, x0, a0 # save this call argument
+    addi sp, sp, -8 # create space on the stack for ra and s0
     sw ra, 0(sp)
     sw s0, 4(sp)
-    addi a0, a0, -1
+    addi a0, a0, -1 # decrement a0 for the next call
     jal factorial
     lw ra, 0(sp)
     lw s0, 4(sp)
-    add t2, x0, a0
-    mul a0, a0, t2
+    addi sp, sp, 8 # restore the stack
+    mul a0, s0, a0 # multiply the reutrned value by this call argument and put in a0 for the previous call
     jr ra
-
 
 exit:
     jr ra
