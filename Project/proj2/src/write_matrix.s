f956f72f0c705945f7bc45d86bf3c4f34cf618bc
Hassan Mahmoud <officialhassansmail@gmail.com>
2025-11-12 13:07:36 +0300

    - solve prject2 partB write_matrix function

--- Project/proj2/src/write_matrix.s
+++ Project/proj2/src/write_matrix.s
@@ -24,17 +24,73 @@
 # ==============================================================================
 write_matrix:
 
-    # Prologue
+    # save the arguments
+    addi sp, sp, -20
+    sw s0, 0(sp)
+    sw s1, 4(sp)
+    sw s2, 8(sp)
+    sw s3, 12(sp)
+    sw ra, 16(sp)
 
+    mv s1, a1 # matrix in memory
+    mv s2, a2 # rows
+    mv s3, a3 # cols
 
+    # open the file to be read
+    mv a1, a0
+    li a2, 1
+    jal fopen
 
+    # check if fopen call failed
+    li t0, -1
+    beq a0, t0, fopen_exit
 
+    # save the file descriptor to s0
+    mv s0, a0
 
+    # compute the number of elements to be written
+    mul t0, s2, s3 
+    addi t0, t0, 2 # t0 = number of elements to be written (+2 for the rows cols)
+    li t1, 4 # sizeof(int)
 
+    mv a1, s0
+    mv a2, s1
+    mv a3, t0
+    mv a4, t1
+    jal fwrite
 
+    # check if fwrite call failed
+    mul t0, s2, s3
+    addi t0, t0, 2
+    bne a0, t0, fwrite_exit
 
+    mv a1, s0
+    jal fclose
 
-    # Epilogue
+    # check if fclose call failed
+    li t0, -1
+    beq a0, t0, fclose_exit
 
+    lw s0, 0(sp)
+    lw s1, 4(sp)
+    lw s2, 8(sp)
+    lw s3, 12(sp)
+    lw ra, 16(sp)
+    addi sp, sp, 20
 
     ret
+
+fopen_exit:
+    # exit with error code 93
+    li a1, 93
+    jal exit2
+
+fwrite_exit:
+    # exit with error code 94
+    li a1, 94
+    jal exit2
+
+fclose_exit:
+    # exit with error code 95
+    li a1, 95
+    jal exit2
