6f3eb41f45e21b5d31f5c3311d30cbabd69567fb
Hassan Mahmoud <officialhassansmail@gmail.com>
2026-01-26 12:35:35 +0300

    - implement a solution for transpose_blocking lab7 ex3 - the solution doesn't make use of the idea of blocking yet

--- Lab/lab07/transpose.c
+++ Lab/lab07/transpose.c
@@ -2,15 +2,19 @@
 
 /* The naive transpose function as a reference. */
 void transpose_naive(int n, int blocksize, int *dst, int *src) {
-    for (int x = 0; x < n; x++) {
-        for (int y = 0; y < n; y++) {
-            dst[y + x * n] = src[x + y * n];
-        }
+  for (int x = 0; x < n; x++) {
+    for (int y = 0; y < n; y++) {
+      dst[y + x * n] = src[x + y * n];
     }
+  }
 }
 
 /* Implement cache blocking below. You should NOT assume that n is a
  * multiple of the block size. */
 void transpose_blocking(int n, int blocksize, int *dst, int *src) {
-    // YOUR CODE HERE
+  for (int x = 0; x < n; x++) {
+    for (int y = 0; y < n; y++) {
+      dst[x + y * n] = src[y + x * n];
+    }
+  }
 }
