cb13c1ffefe2dfecfb2467c680e1f25ce71a16fa
Hassan Mahmoud <officialhassansmail@gmail.com>
2026-02-09 11:56:40 +0300

    - solve allocate_matrix function

--- Project/proj4/matrix.c
+++ Project/proj4/matrix.c
@@ -1,8 +1,8 @@
 #include "matrix.h"
+#include <omp.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <omp.h>
 
 // Include SSE intrinsics
 #if defined(_MSC_VER)
@@ -24,95 +24,128 @@
  * __m256d _mm256_cmp_pd (__m256d a, __m256d b, const int imm8)
  * __m256d _mm256_and_pd (__m256d a, __m256d b)
  * __m256d _mm256_max_pd (__m256d a, __m256d b)
-*/
+ */
 
 /*
  * Generates a random double between `low` and `high`.
  */
 double rand_double(double low, double high) {
-    double range = (high - low);
-    double div = RAND_MAX / range;
-    return low + (rand() / div);
+  double range = (high - low);
+  double div = RAND_MAX / range;
+  return low + (rand() / div);
 }
 
 /*
  * Generates a random matrix with `seed`.
  */
 void rand_matrix(matrix *result, unsigned int seed, double low, double high) {
-    srand(seed);
-    for (int i = 0; i < result->rows; i++) {
-        for (int j = 0; j < result->cols; j++) {
-            set(result, i, j, rand_double(low, high));
-        }
+  srand(seed);
+  for (int i = 0; i < result->rows; i++) {
+    for (int j = 0; j < result->cols; j++) {
+      set(result, i, j, rand_double(low, high));
     }
+  }
 }
 
 /*
- * Allocate space for a matrix struct pointed to by the double pointer mat with
- * `rows` rows and `cols` columns. You should also allocate memory for the data array
- * and initialize all entries to be zeros. Remember to set all fieds of the matrix struct.
- * `parent` should be set to NULL to indicate that this matrix is not a slice.
- * You should return -1 if either `rows` or `cols` or both have invalid values, or if any
- * call to allocate memory in this function fails. If you don't set python error messages here upon
- * failure, then remember to set it in numc.c.
- * Return 0 upon success and non-zero upon failure.
+ * Allocate space for a matrix struct pointed to by the double pointer `mat`
+ * with `rows` rows and `cols` columns. You should also allocate memory for the
+ * data array and initialize all entries to be zeros. Remember to set all fields
+ * of the matrix struct. `parent` should be set to NULL to indicate that this
+ * matrix is not a slice. You should return -1 if either `rows` or `cols` or
+ * both have invalid values, or if any call to allocate memory in this function
+ * fails. If you don't set python error messages here upon failure, then
+ * remember to set it in numc.c. Return 0 upon success and non-zero upon
+ * failure.
  */
 int allocate_matrix(matrix **mat, int rows, int cols) {
-    /* TODO: YOUR CODE HERE */
+
+  if (rows < 1 || cols < 1) {
+    return -1;
+  }
+
+  *mat = malloc(sizeof(matrix));
+  if (*mat == NULL) {
+    return -1;
+  }
+
+  (*mat)->rows = rows;
+  (*mat)->cols = cols;
+  (*mat)->parent = NULL;
+  (*mat)->ref_cnt = 1;
+
+  if (rows == 1 || cols == 1) {
+    (*mat)->is_1d = 1;
+  } else {
+    (*mat)->is_1d = 0;
+  }
+
+  (*mat)->data = malloc(rows * sizeof(double *));
+  if ((*mat)->data == NULL) {
+    free(*mat);
+    return -1;
+  }
+
+  for (int i = 0; i < rows; i++) {
+    (*mat)->data[i] = calloc(cols, sizeof(double));
+    if ((*mat)->data[i] == NULL) {
+      for (int j = 0; j < i; j++) {
+        free((*mat)->data[j]);
+      }
+      free((*mat)->data);
+      free(*mat);
+      return -1;
+    }
+  }
+
+  return 0;
 }
 
 /*
- * Allocate space for a matrix struct pointed to by `mat` with `rows` rows and `cols` columns.
- * This is equivalent to setting the new matrix to be
+ * Allocate space for a matrix struct pointed to by `mat` with `rows` rows and
+ * `cols` columns. This is equivalent to setting the new matrix to be
  * from[row_offset:row_offset + rows, col_offset:col_offset + cols]
- * If you don't set python error messages here upon failure, then remember to set it in numc.c.
- * Return 0 upon success and non-zero upon failure.
+ * If you don't set python error messages here upon failure, then remember to
+ * set it in numc.c. Return 0 upon success and non-zero upon failure.
  */
-int allocate_matrix_ref(matrix **mat, matrix *from, int row_offset, int col_offset,
-                        int rows, int cols) {
-    /* TODO: YOUR CODE HERE */
+int allocate_matrix_ref(matrix **mat, matrix *from, int row_offset,
+                        int col_offset, int rows, int cols) {
+  /* TODO: YOUR CODE HERE */
 }
 
 /*
- * This function will be called automatically by Python when a numc matrix loses all of its
- * reference pointers.
- * You need to make sure that you only free `mat->data` if no other existing matrices are also
- * referring this data array.
+ * This function will be called automatically by Python when a numc matrix loses
+ * all of its reference pointers. You need to make sure that you only free
+ * `mat->data` if no other existing matrices are also referring this data array.
  * See the spec for more information.
  */
-void deallocate_matrix(matrix *mat) {
-    /* TODO: YOUR CODE HERE */
-}
+void deallocate_matrix(matrix *mat) { /* TODO: YOUR CODE HERE */ }
 
 /*
  * Return the double value of the matrix at the given row and column.
  * You may assume `row` and `col` are valid.
  */
-double get(matrix *mat, int row, int col) {
-    /* TODO: YOUR CODE HERE */
-}
+double get(matrix *mat, int row, int col) { /* TODO: YOUR CODE HERE */ }
 
 /*
  * Set the value at the given row and column to val. You may assume `row` and
  * `col` are valid
  */
 void set(matrix *mat, int row, int col, double val) {
-    /* TODO: YOUR CODE HERE */
+  /* TODO: YOUR CODE HERE */
 }
 
 /*
  * Set all entries in mat to val
  */
-void fill_matrix(matrix *mat, double val) {
-    /* TODO: YOUR CODE HERE */
-}
+void fill_matrix(matrix *mat, double val) { /* TODO: YOUR CODE HERE */ }
 
 /*
  * Store the result of adding mat1 and mat2 to `result`.
  * Return 0 upon success and a nonzero value upon failure.
  */
 int add_matrix(matrix *result, matrix *mat1, matrix *mat2) {
-    /* TODO: YOUR CODE HERE */
+  /* TODO: YOUR CODE HERE */
 }
 
 /*
@@ -120,40 +153,37 @@ int add_matrix(matrix *result, matrix *mat1, matrix *mat2) {
  * Return 0 upon success and a nonzero value upon failure.
  */
 int sub_matrix(matrix *result, matrix *mat1, matrix *mat2) {
-    /* TODO: YOUR CODE HERE */
+  /* TODO: YOUR CODE HERE */
 }
 
 /*
  * Store the result of multiplying mat1 and mat2 to `result`.
  * Return 0 upon success and a nonzero value upon failure.
- * Remember that matrix multiplication is not the same as multiplying individual elements.
+ * Remember that matrix multiplication is not the same as multiplying individual
+ * elements.
  */
 int mul_matrix(matrix *result, matrix *mat1, matrix *mat2) {
-    /* TODO: YOUR CODE HERE */
+  /* TODO: YOUR CODE HERE */
 }
 
 /*
  * Store the result of raising mat to the (pow)th power to `result`.
  * Return 0 upon success and a nonzero value upon failure.
- * Remember that pow is defined with matrix multiplication, not element-wise multiplication.
+ * Remember that pow is defined with matrix multiplication, not element-wise
+ * multiplication.
  */
 int pow_matrix(matrix *result, matrix *mat, int pow) {
-    /* TODO: YOUR CODE HERE */
+  /* TODO: YOUR CODE HERE */
 }
 
 /*
  * Store the result of element-wise negating mat's entries to `result`.
  * Return 0 upon success and a nonzero value upon failure.
  */
-int neg_matrix(matrix *result, matrix *mat) {
-    /* TODO: YOUR CODE HERE */
-}
+int neg_matrix(matrix *result, matrix *mat) { /* TODO: YOUR CODE HERE */ }
 
 /*
  * Store the result of taking the absolute value element-wise to `result`.
  * Return 0 upon success and a nonzero value upon failure.
  */
-int abs_matrix(matrix *result, matrix *mat) {
-    /* TODO: YOUR CODE HERE */
-}
-
+int abs_matrix(matrix *result, matrix *mat) { /* TODO: YOUR CODE HERE */ }
